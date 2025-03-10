a10eca360f6cfd79dea2b6defdb952dbfb4803bc
jordenpick <jorde@LAPTOP-MDA8JB3V>
2025-03-10 14:14:39 -0500

    Add project files.

---
+++ Battleship/BasePlayer.cs
@@ -0,0 +1,47 @@
+﻿namespace Battleship
+{
+    class BasePlayer
+    {
+        private Grid grid;
+        private Random rand;
+
+        public BasePlayer()
+        {
+            grid = new Grid();
+            rand = new Random();
+            PlaceShips();
+        }
+
+        public void PlaceShips()
+        {
+            Random rand = new Random();
+            int x = 5;
+            int y = 5;
+
+            string direction = "H";
+            if (rand.Next(0, 2) == 0)
+            {
+                direction = "V";
+            }
+            
+            grid.PlaceShip(new Ship("Battleship", 4), x, y, direction);
+        }
+
+        public bool Attack(Grid enemyGrid)
+        {
+            int x, y;
+            do
+            {
+                x = rand.Next(10);
+                y = rand.Next(10);
+            }
+            while (enemyGrid.MakeGuess(x, y));
+            return true;
+        }
+
+        public Grid GetGrid()
+        {
+            return grid;
+        }
+    }
+}
---
+++ Battleship/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Battleship
+{
+    class Program
+    {
+        static void Main()
+        {
+            BasePlayer player = new BasePlayer();
+            BasePlayer ai = new BasePlayer();
+            Grid aiGrid = ai.GetGrid();
+            Grid playerGrid = player.GetGrid();
+            int shots = 0;
+
+            Console.WriteLine("Welcome to Battleship! Press Enter to start.");
+            Console.ReadLine();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("AI's Board (Shots Fired):");
+                aiGrid.DisplayBoard(false);
+
+                Console.WriteLine("\nYour Board:");
+                playerGrid.DisplayBoard(false);
+                Console.WriteLine("Enter to fire random shots");
+                Console.ReadLine();
+                player.Attack(aiGrid);
+
+                shots++;
+                if (aiGrid.CheckWin())
+                {
+                    Console.WriteLine("You win in " + shots + " shots!");
+                    break;
+                }
+                ai.Attack(playerGrid);
+                if (playerGrid.CheckWin())
+                {
+                    Console.WriteLine("AI wins!");
+                    break;
+                }
+            }
+        }
+    }
+
+
+}
